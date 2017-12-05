5025871132b7ae50f3a77a11dd0a9c2e8ce5c3e2
AK-10 <crossover.ak@outlook.jp>
2017-12-05 16:10:25 +0900

    Initial Commit

---
+++ qiitaViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qiitaViewer
+//
+//  Created by Atsushi KONISHI on 2017/12/05.
+//  Copyright © 2017年 小西篤志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
